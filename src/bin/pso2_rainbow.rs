b13ba4afd669ea35b3db337fdc49d566a4cf929b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-12-12 08:50:52 -0800

    Build basic string generator

--- src/bin/pso2_rainbow.rs
+++ src/bin/pso2_rainbow.rs
@@ -1,6 +1,11 @@
+use std::iter;
+
 use self::models::*;
 use diesel::prelude::*;
+use itertools::Itertools;
 use pso2_rainbow::*;
+
+const CHARSET: &str = "abcdefghijklmnopqrstuvwxyz0123456789/_.";
 
 fn main() {
     use self::schema::hash_mapping::dsl::*;
@@ -13,5 +18,18 @@ fn main() {
     println!("Displaying {} hashes", results.len());
     for hash in results {
         println!("{:?}:{}", hash.md5, hash.filename);
+    }
+
+    let strings = (1..3).fold(
+        Box::new(iter::empty::<String>()) as Box<dyn Iterator<Item = String>>,
+        |it, n| {
+            Box::new(it.chain(CHARSET.chars().permutations(n).unique().map(|v| {
+                String::from_utf8(v.into_iter().map(|c| c as u8).collect_vec())
+                    .expect("ASCII char tuples should always resolve to an owned string")
+            })))
+        },
+    );
+    for string in strings {
+        println!("{}", string)
     }
 }
